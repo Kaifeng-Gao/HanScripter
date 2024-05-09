02db3dc3118bcec985b3522aeffd19b69aeb5e34
Kaifeng Gao <kaifeng.gao@outlook.com>
2024-05-09 00:10:39 -0400

    fix bugs in evaluate script

--- model_evaluate.sh
+++ model_evaluate.sh
@@ -9,23 +9,18 @@ module purge
 module load miniconda
 conda activate cpsc577
 
-python llama_evaluate.py --new_model_path "./results/Llama-3-Han-0422" --finetune True --num_shots 5
-python llama_evaluate.py --new_model_path "./results/Llama-3-Han-0423" --finetune True --num_shots 5
-python llama_evaluate.py --num_shots 5
+python llama_evaluate.py \
+    --model_path "KaifengGGG/Llama3-8b-Hanscripter" \
+    --dataset_path "KaifengGGG/WenYanWen_English_Parallel" \
+    --dataset_config "instruct" \
+    --num_shots 6
 
-python llama_evaluate.py --new_model_path "./results/Llama-3-Han-0422" --finetune True --num_shots 3
-python llama_evaluate.py --new_model_path "./results/Llama-3-Han-0423" --finetune True --num_shots 3
-python llama_evaluate.py --num_shots 3
+# python gemini_evaluate.py \
+#     --dataset_path "KaifengGGG/WenYanWen_English_Parallel" \
+#     --dataset_config "instruct" \
+#     --num_shots 6
 
-python llama_evaluate.py --new_model_path "./results/Llama-3-Han-0422" --finetune True --num_shots 1
-python llama_evaluate.py --new_model_path "./results/Llama-3-Han-0423" --finetune True --num_shots 1
-python llama_evaluate.py --num_shots 1
-
-python llama_evaluate.py --new_model_path "./results/Llama-3-Han-0422" --finetune True --num_shots 0
-python llama_evaluate.py --new_model_path "./results/Llama-3-Han-0423" --finetune True --num_shots 0
-python llama_evaluate.py --num_shots 0
-
-# python llama_evaluate.py --new_model_path "./results/Llama-3-Han" --finetune True
-# python llama_evaluate.py --new_model_path "./results/Llama-3-Han-Instruct" --finetune True
-# python llama_evaluate.py
-# python gemini_evaluate.py
+# python gpt_evaluate.py \
+#     --dataset_path "KaifengGGG/WenYanWen_English_Parallel" \
+#     --dataset_config "instruct" \
+#     --num_shots 6
